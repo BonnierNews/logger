7ba69566205df9e4bfdea1ea8f93d9f6e68f82bd
Mattias Olla <mattias.olla@bonniernews.se>
2024-11-06 13:27:00 +0100

    Expose `getStore`, so users can get all data at any time

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { fetchGcpProjectId } from "./lib/gcp";
 export { getHttpTraceHeader } from "./lib/http";
 export { getLoggingTraceData, logger } from "./lib/logging";
-export { middleware } from "./lib/middleware";
+export { getStore, middleware } from "./lib/middleware";
