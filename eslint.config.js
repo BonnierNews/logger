18ae2730d50178b20ba74d31a152ebb7f2b4b67f
Mattias Olla <mattias.olla@bonniernews.se>
2024-11-06 11:11:33 +0100

    Downgrade eslint to version 8 to use bonniernews eslint config

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-import js from "@eslint/js";
-import ts from "typescript-eslint";
-
-export default ts.config(js.configs.recommended, ...ts.configs.recommended, ...ts.configs.strict, {
-  ignores: ["coverage", "dist"],
-});
