cf28c33a776d61e325511bcf05d944fa18454f67
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2024-11-08 14:21:09 +0100

    Future proofing isSampled

--- lib/traceparent.ts
+++ lib/traceparent.ts
@@ -6,6 +6,11 @@
  * base16(parent-id) = 00f067aa0ba902b7
  * base16(trace-flags) = 00  // 00 is not sampled, 01 is sampled
  */
+function isSampled (traceFlags) {
+  const FLAG_SAMPLED = 0b00000001;
+  return (parseInt(traceFlags, 16) & FLAG_SAMPLED) === FLAG_SAMPLED;
+}
+
 export function getTraceFromTraceparent(traceHeader: string) {
   const parts = traceHeader.split("-");
 
@@ -14,6 +19,6 @@ export function getTraceFromTraceparent(traceHeader: string) {
   return {
     traceId: parts[1],
     parentId: parts[2],
-    isSampled: parts[3] !== "00",
+    isSampled: isSampled(parts[3]),
   };
 }
