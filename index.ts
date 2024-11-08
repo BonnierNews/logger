4aa7f4f7bae4762c70ba11d4f8f5c92775938557
kristofferjansson <kristoffer.jansson@bonniernews.se>
2024-11-08 11:38:31 +0100

    Expose createTraceparent in the public API

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { fetchGcpProjectId } from "./lib/gcp";
 export { getHttpTraceHeader } from "./lib/http";
 export { getLoggingTraceData, logger } from "./lib/logging";
 export { middleware } from "./lib/middleware";
+export { createTraceparent } from "./lib/traceparent";
