6aade1fb66894a7ec0a16140e45e74e8317774e1
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2024-11-13 14:25:37 +0100

    Remove undefined function in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { getTraceparentObject, getTraceparent, getTraceId } from "./lib/propagation";
+export { getTraceparent, getTraceId } from "./lib/propagation";
 export { decorateLogs, logger, Logger, LoggerOptions, DestinationStream } from "./lib/logging";
 export { middleware } from "./lib/middleware";
 export { createTraceparent } from "./lib/traceparent";
